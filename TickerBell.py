ddedf5084daac600b58e35eb96b252899f92f46b
dsmicken <dsmicken@ncsu.edu>
2020-05-11 12:14:19 -0400

    closes #9. Help page can later be expanded to include examples.

--- TickerBell.py
+++ TickerBell.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import yahoo_fin.stock_info as si
 import multiprocessing
+#import textwrap
 import time
 import Alert
 import Price
@@ -22,6 +23,66 @@ def usage():
   print("    price [ticker]")
   print("    quit")
   print()
+  
+def help():
+  """Prints help functions for all commands"""
+  #alert command
+  print("ALERT:\n")
+  print("    The alert command lets you manage alerts and the alert system.")
+  print("\n    CREATE\n")
+  print("        create lets you create a brand new alert.")
+  print("        It requires four parameters.\n")
+  print("        ticker    - the ticker symbol for the stock you want to create an alert for")
+  print("        price     - the price at which, if hit, the alert will go off")
+  print("        less/more - the direction of price movement that will sound the alert")
+  print("                  - ex. 'less' will cause the alert to go off if price is less")
+  print("                    than or equal to the live stock price")
+  print("                  - this is an optional argument that defaults to 'less'")
+  print("        on/off    - the status of the alert. The alert will only occur if the status")
+  print("                    is 'on'.")
+  print("\n    ON/OFF\n")
+  print("        on/off lets you turn an alert on or off.")
+  print("        It requires one parameter.\n")
+  print("        ID    - the ID of the existing alert to be turned on or off")
+  print("\n    PRINT\n")
+  print("        print lets you print out saved alerts, email addresses, or phone numbers.")
+  print("        It requires one parameter.\n")
+  print("        choice    - the data you want to print. Options: alerts, emails, or numbers")
+  print("\n    START/STOP\n")
+  print("        start/stop lets you start or stop the alert system.")
+  print("\n    MODE\n")
+  print("        mode lets you turn on or off different alert modes.")
+  print("        It requires two parameters.\n")
+  print("        mode      - the alert mode that you want to turn on/off. Options: cli, email, text")
+  print("        on/off    - the status of the mode. The alert will be sent through this mode")
+  print("                    only if the status is 'on")
+  print("\n    EMAIL\n")
+  print("        email lets you add or remove an email to/from the alert system.")
+  print("        It requires two parameters.\n")
+  print("        add/remove    - whether you want to save or delete a saved email address")
+  print("        address       - the email address to save or delete")
+  print("\n    PHONE\n")
+  print("        phone lets you add or remove a phone number to/from the alert system.")
+  print("        It requires three parameters.\n")
+  print("        add/remove    - whether you want to save or delete a saved phone number")
+  print("        phone number  - the phone number to save or delete")
+  print("        carrier       - the service carrier for the phone number with all spaces removed")
+  print("")
+  print("IO:\n")
+  print("    The io command lets you manage input/output with files")
+  print("\n    IMPORT/EXPORT\n")
+  print("        import/export lets you resume/save current TickerBell system states.")
+  print("        It requires one parameter.\n")
+  print("        filname  - the name of the file to import from or export to")
+  print("")
+  print("PRICE:\n")
+  print("    The price command lets you view live stock prices")
+  print("    There are no subcommands within price. It takes one argument.\n")
+  print("    ticker - the ticker symbol to check the price of")
+  print("")
+  print("QUIT:")
+  print("    The quit command ends the current session.")
+  print("\n")
 
 def printBanner():
   """prints the TickerBell welcome banner""" 
@@ -56,6 +117,9 @@ def handleInput(inpt):
 
   elif ( cmd == "io" ):
     TIO.handleIO(inpt[3:])
+
+  elif (cmd == "help" ):
+    help()
       
   elif ( cmd != "quit" ):
     print("Invalid Input")
@@ -63,7 +127,7 @@ def handleInput(inpt):
   
 
 def main():
-
+  help()
   # Introduction Message and Input
   printBanner()
   usage()
